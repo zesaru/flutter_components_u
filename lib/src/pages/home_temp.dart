1dd1f4386619c4ee8f622bfd3a1ac35635a16ae7
Cesar Murillo <namiayumis@gmail.com>
2019-08-15 15:48:14 +0900

    ListVIew-for-..

--- lib/src/pages/home_temp.dart
+++ lib/src/pages/home_temp.dart
@@ -1,27 +1,29 @@
 import 'package:flutter/material.dart';
 
 class HomePageTemp extends StatelessWidget {
+  final opciones = ['uno', 'dos', 'tres', 'cuatro', 'cinco', 'seis'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Componenet Temp'),
       ),
-      body: ListView(
-        children: <Widget>[
-          ListTile(
-            title: Text('ListTile Title'),
-          ),
-          Divider(),
-          ListTile(
-            title: Text('ListTile Title'),
-          ),
-          Divider(),
-          ListTile(
-            title: Text('ListTile Title'),
-          ),
-        ],
-      ),
+      body: ListView(children: _crearItems()),
     );
+  }
+
+  List<Widget> _crearItems() {
+    List<Widget> lista = List<Widget>();
+
+    for (String opt in opciones) {
+      final tempWidget = ListTile(
+        title: Text(opt),
+      );
+
+      lista..add(tempWidget)..add(Divider());
+    }
+
+    return lista;
   }
 }
