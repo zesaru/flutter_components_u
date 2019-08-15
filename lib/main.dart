c5596fc415b0e28d3bf4ce4ff86e1ee43bfe4936
Cesar Murillo <namiayumis@gmail.com>
2019-08-15 11:16:38 +0900

    start component proyect

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:components/src/pages/home_temp.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Componentes APP',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Componentes'),
+        ),
+        body: HomePageTemp(),
+      ),
+    );
+  }
+}
