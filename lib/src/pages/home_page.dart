beebdd6242a2fffd04f9ef5d4c184a09b9baed3d
Cesar Murillo <namiayumis@gmail.com>
2019-08-16 17:15:31 +0900

    Future Builder

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -13,20 +13,34 @@ class HomePage extends StatelessWidget {
   }
 
   Widget _lista() {
-    print(menuProvider.opciones);
+    return FutureBuilder(
+      future: menuProvider.cargarData(),
+      //initialData: InitialData,
+      builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
+        return ListView(
+          children: _listaItems(snapshot.data),
+        );
+      },
+    );
 
     return ListView(
       children: _listaItems(),
     );
   }
 
-  List<Widget> _listaItems() {
-    return [
-      ListTile(title: Text('1')),
-      Divider(),
-      ListTile(title: Text('1')),
-      Divider(),
-      ListTile(title: Text('1')),
-    ];
+  List<Widget> _listaItems(List<dynamic> data) {
+    final List<Widget> opciones = [];
+
+    data.forEach((opt) {
+      final widgetTemp = ListTile(
+        title: Text(opt['texto']),
+        leading: Icon(Icons.account_circle, color: Colors.blue),
+        trailing: Icon(Icons.arrow_forward_ios, color: Colors.blue),
+        onTap: () {},
+      );
+      opciones..add(widgetTemp)..add(Divider());
+    });
+
+    return opciones;
   }
 }
