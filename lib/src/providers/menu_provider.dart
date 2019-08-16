f4b3be2710e9162d1c758992a55e45763ac46b4d
Cesar Murillo <namiayumis@gmail.com>
2019-08-16 11:04:14 +0900

    json convert map

--- lib/src/providers/menu_provider.dart
+++ lib/src/providers/menu_provider.dart
@@ -1,16 +1,22 @@
+import 'dart:convert';
+
 import 'package:flutter/services.dart';
 
 class _MenuProvider {
   List<dynamic> opciones = [];
 
   _MenuProvider() {
-    cargarData();
+    //cargarData();
   }
 
-  cargarData() {
-    rootBundle.loadString('data/menu_opts.json').then((data) {
-      print(data);
-    });
+  Future<List<dynamic>> cargarData() async {
+    final resp = await rootBundle.loadString('data/menu_opts.json');
+
+    Map dataMap = json.decode(resp);
+    print(dataMap['rutas']);
+    opciones = dataMap['rutas'];
+
+    return opciones;
   }
 }
 
