161813832b46f283b70405067f3aaf66b04ef42c
Cesar Murillo <namiayumis@gmail.com>
2019-08-18 18:19:51 +0900

    pages pop navigator

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:components/src/pages/alert_page.dart';
 import 'package:components/src/providers/menu_provider.dart';
 import 'package:components/src/utils/icon_string_util.dart';
 import 'package:flutter/material.dart';
@@ -19,13 +20,13 @@ class HomePage extends StatelessWidget {
       //initialData: InitialData,
       builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
         return ListView(
-          children: _listaItems(snapshot.data),
+          children: _listaItems(snapshot.data, context),
         );
       },
     );
   }
 
-  List<Widget> _listaItems(List<dynamic> data) {
+  List<Widget> _listaItems(List<dynamic> data, BuildContext context) {
     final List<Widget> opciones = [];
 
     data.forEach((opt) {
@@ -33,7 +34,10 @@ class HomePage extends StatelessWidget {
         title: Text(opt['texto']),
         leading: getIcon(opt['icon']),
         trailing: Icon(Icons.arrow_forward_ios, color: Colors.blue),
-        onTap: () {},
+        onTap: () {
+          final route = MaterialPageRoute(builder: (context) => AlertPage());
+          Navigator.push(context, route);
+        },
       );
       opciones..add(widgetTemp)..add(Divider());
     });
