3e476b4a97d47b282fe7026d4ac0aec432e19bd7
Cesar Murillo <namiayumis@gmail.com>
2019-08-20 16:01:25 +0900

    Circle Avatar

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -6,6 +6,31 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page'),
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.all(7.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://www.biography.com/.image/t_share/MTIwNjA4NjM0MTk3MjE0NzMy/stan-lee-21101093-1-402.jpg'),
+              radius: 25.0,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text('SL'),
+              backgroundColor: Colors.brown,
+            ),
+          ),
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          image: NetworkImage(
+              'https://img-cdn.hipertextual.com/files/2019/04/hipertextual-avengers-endgame-contiene-ultimo-cameo-stan-lee-2019632812.jpg?strip=all&lossy=1&quality=57&resize=740%2C490&ssl=1'),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          fadeInDuration: Duration(milliseconds: 200),
+        ),
       ),
     );
   }
