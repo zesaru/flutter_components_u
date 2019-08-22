addf7fa01dee6fd56272c75b156bd30de528eb5e
Cesar Murillo <namiayumis@gmail.com>
2019-08-22 12:35:06 +0900

    change language datepicker

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:components/src/pages/alert_page.dart';
 import 'package:components/src/routes/routes.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,6 +12,15 @@ class MyApp extends StatelessWidget {
       title: 'Componentes APP',
       debugShowCheckedModeBanner: false,
       //home: HomePage(),
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('en'), // English
+        const Locale('es'),
+      ],
       routes: getApplicationRoutes(),
       onGenerateRoute: (RouteSettings settings) {
         print('Ruta llamada: ${settings.name}');
--- lib/src/pages/input_page.dart
+++ lib/src/pages/input_page.dart
@@ -130,11 +130,11 @@ class _InputPageState extends State<InputPage> {
 
   _selectDate(BuildContext context) async {
     DateTime picked = await showDatePicker(
-      context: context,
-      initialDate: new DateTime.now(),
-      firstDate: new DateTime(2018),
-      lastDate: new DateTime(2020),
-    );
+        context: context,
+        initialDate: new DateTime.now(),
+        firstDate: new DateTime(2018),
+        lastDate: new DateTime(2020),
+        locale: Locale('es', 'ES'));
     if (picked != null) {
       setState(() {
         _fecha = picked.toString();
